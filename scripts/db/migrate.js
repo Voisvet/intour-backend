4ce549f462d35a74cbb602db1b49401b5c9de958
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2019-03-10 22:36:59 +0300

    init sequelize infrastructure

---
+++ scripts/db/migrate.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const spawn = require('child-process-promise').spawn;
+
+const spawnOptions = { stdio: 'inherit' };
+
+const env_mode = process.env.ENV || 'development';
+
+(async () => {
+  try {
+    // Migrate the DB
+    await spawn('./node_modules/.bin/sequelize', ['db:migrate', `--env=${env_mode}`], spawnOptions);
+    console.log('*************************');
+    console.log('Migration successfulnpm a');
+  } catch (err) {
+    // Oh no!
+    console.log('*************************');
+    console.log('Migration failed. Error:', err.message);
+    process.exit(1);
+  }
+
+  process.exit(0);
+})();
