66ed0e8657515f92353583dd0dcb0aded28cae87
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2019-03-11 22:56:45 +0300

    add: (almost) implement /excursions endpoint

--- routes/public.js
+++ routes/public.js
@@ -1,12 +1,43 @@
+const db = require('../models');
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({
-    status: 0,
-    message: 'This is an endpoint for mobile app'
-  });
+weekDays = [
+  'sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'
+];
+
+router.get('/excursions', function(req, res, next) {
+  const date = new Date(+req.query.date);
+  db.sequelize.model('Excursion').findAll({
+    where: {
+      region: req.query.region,
+    },
+    include: [{
+      model: db.sequelize.model('ExcursionImage'),
+      as: 'Images'
+    }, {
+      model: db.sequelize.model('ExcursionSchedule'),
+      as: 'Schedule',
+      where: {
+        weekDay: weekDays[date.getDay()]
+      }
+    }]
+  })
+    .then(result => {
+      const excursions = result.map(excursion => {
+        return {
+          id: excursion.id,
+          title: excursion.title,
+          price: excursion.adult_ticket_cost,
+          start_time: excursion.Schedule[0].time,
+          duration: excursion.duration,
+          services: excursion.services,
+          image: excursion.Images[0].link,
+          type: ''
+        };
+      });
+      res.send(excursions);
+    });
 });
 
 module.exports = router;
