7948c4b8327177b2752db69283f73c26036528a1
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2019-03-21 22:28:16 +0300

    implement customer creation

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,90 @@
+const db = require('../models');
+const helpers = require('../lib/helpers');
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({
-    status: 0,
-    errorMessage: 'This is an endpoint for mobile app'
-  });
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+
+function checkString(string) {
+  return typeof(string) == "string"
+    && string.trim().length > 0
+    ? string.trim() : false;
+}
+
+router.post('/new', async (req, res, next) => {
+  const firstName = checkString(req.body.first_name);
+  const lastName = checkString(req.body.last_name);
+  const phone = checkString(req.body.phone);
+  const password = checkString(req.body.password);
+  const agentId = req.body.agent_id;
+
+  if (firstName && lastName && phone && password) {
+    let transaction;
+
+    try {
+      transaction = await db.sequelize.transaction();
+
+      let agent = null;
+      // Get agent if it is specified
+      if (typeof(agentId) == 'number') {
+        agent = await db.sequelize.model('Agent')
+          .findByPk(agentId, {transaction});
+        if (!agent) {
+          res.send({
+            status: -1,
+            errorMessage: 'Agent with specified ID is not found'
+          });
+          await transaction.rollback();
+          return;
+        }
+      }
+
+      // Create new customer
+      const customer = await db.sequelize.model('Customer').create({
+        firstName,
+        lastName,
+        phone
+      }, {transaction});
+
+      // Add new customer to agent's list of customers
+      if (agent) await agent.addCustomers(customer, {transaction});
+
+      // Create new user
+      const user = await db.sequelize.model('User').create({
+        login: phone,
+        passwordHash: helpers.hash(password),
+        accountType: 'customer'
+      }, {transaction});
+
+      // Associate new user with a customer
+      await user.setCustomer(customer, {transaction});
+
+      await transaction.commit();
+
+      res.send({
+        status: 0,
+        errorMessage: '',
+        token: jwt.sign({
+          userId: user.id,
+          customerId: customer.id,
+          accountType: user.accountType
+        }, config.tokenSecret, {
+          expiresIn: '24h'
+        })
+      });
+    } catch (err) {
+      if (err && transaction) await transaction.rollback();
+      throw new Error('Something went wrong when stored data to DB');
+    }
+
+  } else {
+    res.send({
+      status: -1,
+      errorMessage: 'Missing required fields'
+    });
+  }
 });
 
 module.exports = router;
