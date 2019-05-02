51a6d6185bbb5cf5b61f694695447573384dcd09
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2019-05-02 12:55:29 +0300

    delete logging and add cleanup for payment link

--- routes/service.js
+++ routes/service.js
@@ -12,9 +12,6 @@ router.get('/returning_url', (req, res) => {
 });
 
 router.post('/yk_notification', async (req, res) => {
-  console.log('[SERV] Got notification from YK');
-  console.log(req.body);
-
   const type = typeof(req.body.type) == 'string' && req.body.type === 'notification' ?
     req.body.type : false;
   const paymentId = typeof(req.body.object.id) == 'string' && req.body.object.id.length > 0 ?
@@ -25,16 +22,9 @@ router.post('/yk_notification', async (req, res) => {
     && +req.body.object.metadata.reservationId > 0 ?
     +req.body.object.metadata.reservationId : false;
 
-  console.log('type', type);
-  console.log('paymentId', paymentId);
-  console.log('paymentStatus', paymentStatus);
-  console.log('reservationId', reservationId);
-
-
   if (reservationId && type && paymentId && paymentStatus &&
       typeof(req.body.object.paid) =='boolean') {
 
-    console.log('[SERV] Passed all checks');
     const reservation = await db.sequelize.model('Reservation').findByPk(reservationId);
     const response = await axios.get(
       config.yk.kassaApiUrl + '/payments/' + paymentId,
@@ -45,9 +35,6 @@ router.post('/yk_notification', async (req, res) => {
         }
       }
     );
-
-    console.log('[SERV] Requested payment from server');
-    console.log(response);
 
     if (response.status !== 200) {
       res.status(500).send();
@@ -64,14 +51,12 @@ router.post('/yk_notification', async (req, res) => {
     if (req.body.object.paid && reservation.status !== 'paid') {
       reservation.status = 'paid';
       reservation.paymentDate = new Date();
+      reservation.paymentLink = '';
     }
     await reservation.save();
 
-    console.log('[SERV] Reservation updated');
-
     res.status(200).send();
   } else {
-    console.log('[SERV] Request is not valid');
     res.status(400).send();
   }
 });
